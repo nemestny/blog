ec881fabb83e3565ce996183bfe6752fdf611431
Aleksandr Noskov <nemestny@gmail.com>
2018-09-27 20:15:01 +0500

    move indexes in migration file

--- db/migrate/20180927150738_create_users.rb
+++ db/migrate/20180927150738_create_users.rb
@@ -1,15 +1,13 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email
+      t.string :name, index: { unique: true }
+      t.string :email, index: { unique: true }
       t.boolean :moderator
       t.boolean :creator
       t.boolean :banned
 
       t.timestamps
     end
-    add_index :users, :name, unique: true
-    add_index :users, :email, unique: true
   end
 end
