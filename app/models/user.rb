eacbb8f91fbb24bfbdf76345b0e72bee95810b91
Aleksandr Noskov <nemestny@gmail.com>
2018-10-01 13:24:28 +0500

    Add association :commented_posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :posts 
   has_many :comments
+  has_many :commented_posts, through: :comments, source: :post
 
   validates :name, presence: true
   validates :name, length: { minimum: 2, maximum: 16 }
