c773a370d385866e41f92f30718ac9beaed05323
Aleksandr Noskov <nemestny@gmail.com>
2018-10-01 13:13:40 +0500

    Minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,13 @@ User.destroy_all
 
 hash_users = 10.times.map do
   {
-  name: FFaker::Internet.user_name[0...16],
-  email: FFaker::Internet.safe_email
+    name: FFaker::Internet.user_name[0...16],
+    email: FFaker::Internet.safe_email
   }
 end
 
 users = User.create! hash_users
+
 users.first(7).each { |u| u.update creator: true }
 users.first(2).each { |u| u.update moderator: true }
 
@@ -17,9 +18,9 @@ creators = User.where(creator: true)
 
 hash_posts = 20.times.map do
   {
-  title: FFaker::HipsterIpsum.paragraph,
-  body: FFaker::HipsterIpsum.paragraphs,
-  user: creators.sample
+    title: FFaker::HipsterIpsum.paragraph,
+    body: FFaker::HipsterIpsum.paragraphs,
+    user: creators.sample
   }
 end
 
@@ -27,9 +28,9 @@ posts = Post.create! hash_posts
 
 hash_comments = 200.times.map do
   {
-  body: FFaker::HipsterIpsum.paragraphs,
-  user: users.sample,
-  post: posts.sample
+    body: FFaker::HipsterIpsum.paragraphs,
+    user: users.sample,
+    post: posts.sample
   }
 end
 
