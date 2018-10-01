98b0810eaf90b80b3e778ca1d9f53a0cd8b200be
Aleksandr Noskov <nemestny@gmail.com>
2018-10-01 10:28:07 +0500

    Use seed.rb with ffaker

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :post
   
   validates :body, presence: true
-  validates :body, length: { minimum: 2, maximum: 1000}
+  validates :body, length: { minimum: 2, maximum: 3000}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,9 @@ class Post < ApplicationRecord
   has_many :comments
   
   validates :title, presence: true
-  validates :title, length: { minimum: 2, maximum: 16 }
+  validates :title, length: { minimum: 2, maximum: 500 }
   validates :title, uniqueness: true
 
   validates :body, presence: true
-  validates :body, length: { minimum: 2, maximum: 1000}
+  validates :body, length: { minimum: 2, maximum: 3000}
 end
