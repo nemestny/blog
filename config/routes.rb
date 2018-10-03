f08698f1751503366966d1bf194b4486dcd1c600
Aleksandr Noskov <nemestny@gmail.com>
2018-10-03 20:27:09 +0500

    Use scaffold generate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :posts
   root 'home#index'
   get 'about', to:'home#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
